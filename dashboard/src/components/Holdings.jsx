b658cfa42a3c63a69ce64bcb12a9b284f144e4f3
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-04-23 12:48:59 +0530

    Watchlist and Positions completed

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,25 +1,49 @@
 import React from "react";
-
+import { holdings } from "../data/data";
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Holdings ({ holdings.length })</h3>
 
       <div className="order-table">
-        <table>
-          <tr>
-            <th>Instrument</th>
-            <th>Qty.</th>
-            <th>Avg. cost</th>
-            <th>LTP</th>
-            <th>Cur. val</th>
-            <th>P&L</th>
-            <th>Net chg.</th>
-            <th>Day chg.</th>
-          </tr>
-        </table>
-      </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Instrument</th>
+        <th>Qty.</th>
+        <th>Avg. cost</th>
+        <th>LTP</th>
+        <th>Cur. val</th>
+        <th>P&L</th>
+        <th>Net chg.</th>
+        <th>Day chg.</th>
+      </tr>
+    </thead>
 
+    <tbody>
+      {holdings.map((stock, index) => {
+        const currValue = stock.price * stock.qty;
+        const pnl = currValue - stock.avg * stock.qty;
+
+        const profClass = pnl >= 0 ? "profit" : "loss";
+        const dayClass = stock.isLoss ? "loss" : "profit";
+
+        return (
+          <tr key={index}>
+            <td>{stock.name}</td>
+            <td>{stock.qty}</td>
+            <td>{stock.avg.toFixed(2)}</td>
+            <td>{stock.price.toFixed(2)}</td>
+            <td>{currValue.toFixed(2)}</td>
+            <td className={profClass}>{pnl.toFixed(2)}</td>
+            <td className={profClass}>{stock.net}</td>
+            <td className={dayClass}>{stock.day}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+</div>
       <div className="row">
         <div className="col">
           <h5>
