57555b5d7ea5d84900ea3ce8f019c25082327497
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-04-27 13:33:09 +0530

    Buy and Sell Popups

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,59 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const {HoldingsModel} = require("./model/HoldingsModel");
-const {PositionsModel} = require("./model/PositionsModel");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 
 const PORT = process.env.PORT || 8080;
 const URI = process.env.MONGO_URL;
-
 const app = express();
 
-mongoose.connect(URI)
-.then(() => {
-    console.log("MongoDB Connected");
+app.use(cors());
+app.use(bodyParser.json());
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-})
-.catch((err) => {
-    console.log("DB Connection Error:", err);
+app.get("/allHoldings", async (req, res) => {
+  try {
+    const allHoldings = await HoldingsModel.find({});
+    res.json(allHoldings);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch holdings" });
+  }
 });
+
+app.get("/allPositions", async (req, res) => {
+  try {
+    const allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch positions" });
+  }
+});
+
+app.post("/newOrder", async(req,res)=>{
+  let newOrder = new OrdersModel({
+    name : req.body.name,
+    qty : req.body.qty,
+    price : req.body.price,
+    mode : req.body.mode,
+  });
+
+  newOrder.save();
+  res.send("OrderSaved");
+  
+})
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB Connection Error:", err);
+  });
